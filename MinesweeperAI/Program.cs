3aac43611801ecea6f34b27f44fe104bd9aa5634
Jonathan Hsieh <jonathanh1386@gmail.com>
2018-04-19 06:41:42 -0700

    Nearly completed Game.cs

---
+++ MinesweeperAI/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MinesweeperAI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
